184e3eced09005b3a1f12851fc639fc066112c7c
Sihal Munidasa <sihal.munidasa03@gmail.com>
2025-01-14 21:34:01 +0530

    Created a class called Movie which a Director has
    Also learned something about default int values in the case for instance variables vs local variables.

--- src/relationships/Test.java
+++ src/relationships/Test.java
@@ -1,23 +1,34 @@
 package relationships;
 
 /*
- * A test class for the Director class.
+ * A test program for the Movie	class.
  */
 public class Test {
     public static void main(String[] args) {
-        // Test constructor
-        Director director = new Director("James", "Cameron");
 
-        // Test Setters and Getters
-
-        // Crete an object Date to represent the dob
+        // We need a Director instance to create a Movie instance
+        Director director =	new	Director("James",	"Cameron");
         Date dob = new Date(16, 8, 1954);
         director.setDOB(dob);
         director.setNoOfMovies(23);
-        System.out.println(director);  // toString()
-        System.out.println("name is: " + director.getName());
-        System.out.println("surname is: " + director.getSurname());
-        System.out.println("dob is: " + director.getDOB().getDate());
-        System.out.println("number of directed movies is: " + director.getNoOfMovies());
+        System.out.println(director); // Director's	toString()
+
+        // Test Movie's	constructor	and	toString()
+        Movie movie	= new Movie("Avatar", "Fantasy", director);
+        System.out.println(movie); // Movie's toString()
+        // Here when we print out the movie, the number of awards is mentioned as 0
+        // but we never initialised the numAwards variable
+        // It is because when an instance is created of an object that includes an attribute with
+        // data type int, it sets it's value to 0 by default
+        // However, this is not the case for local variables (the ones in methods)
+
+        // Test Setters and Getters
+        movie.setNumAward(23);
+        System.out.println(movie); // Movie's toString()
+        System.out.println("title is: "	+ movie.getTitle());
+        System.out.println("category is: " + movie.getCategory());
+        System.out.println("name of	director is: " + movie.getDirector().getName());
+        System.out.println("surname	of director is: " + movie.getDirector().getSurname());
+        System.out.println("number of awards is: " + movie.getNumAward());
     }
 }
